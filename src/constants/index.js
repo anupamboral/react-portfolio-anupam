4c252057f60ced7c37553e045493fb0bd4565bff
anupamboral <anupamboral6889@gmail.com>
2025-02-20 04:44:58 +0530

    removed unnecessary data

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,36 +8,36 @@ export const HERO_CONTENT = `I am a passionate  frontend developer with a knack 
 
 export const ABOUT_TEXT = `I am a dedicated and versatile frontend developer with a passion for creating efficient and user-friendly web applications. With 1.5 years of project based experience, I have worked with a variety of technologies, including React.js. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active,playing guitar, exploring new technologies, and in future contributing to open-source projects.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+// export const EXPERIENCES = [
+//   {
+//     year: "2023 - Present",
+//     role: "Senior Full Stack Developer",
+//     company: "Google Inc.",
+//     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+//     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+//   },
+//   {
+//     year: "2022 - 2023",
+//     role: "Frontend Developer",
+//     company: "Adobe",
+//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+//     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+//   },
+//   {
+//     year: "2021 - 2022",
+//     role: "Full Stack Developer",
+//     company: "Facebook",
+//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+//     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+//   },
+//   {
+//     year: "2020 - 2021",
+//     role: "Software Engineer",
+//     company: "Paypal",
+//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+//     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+//   },
+// ];
 
 export const PROJECTS = [
   {
@@ -75,7 +75,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["React.js", "Tailwind CSS", "Vite", "Framer Motion"],
-    url: "https://anupam-portfolio.netlify.app/",
+    url: "https://react-portfolio-anupam.netlify.app/",
   },
   {
     title: "Forkify",
